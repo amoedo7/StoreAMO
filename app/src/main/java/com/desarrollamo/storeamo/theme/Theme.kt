4c15c1b8a023e878ae0c377b9ac0b732fe32ae7a
El3imm <74299901+amoedo7@users.noreply.github.com>
2026-08-19 08:53:47 +0200

    Fix theme luminance import

--- app/src/main/java/com/desarrollamo/storeamo/theme/Theme.kt
+++ app/src/main/java/com/desarrollamo/storeamo/theme/Theme.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
