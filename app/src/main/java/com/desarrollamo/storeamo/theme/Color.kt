9c05f63a973611acf4a0cdc8379e313aca6fa069
El3imm <74299901+amoedo7@users.noreply.github.com>
2026-08-19 08:06:46 +0200

    Add DesarrollAMO brand colors

---
+++ app/src/main/java/com/desarrollamo/storeamo/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.desarrollamo.storeamo.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AmoBackground = Color(0xFF06101C)
+val AmoBackground2 = Color(0xFF091727)
+val AmoSurface = Color(0xFF0D1D30)
+val AmoSurface2 = Color(0xFF12263D)
+val AmoLine = Color(0xFF203A58)
+val AmoText = Color(0xFFF7F9FC)
+val AmoMuted = Color(0xFF8FA5BC)
+val AmoCyan = Color(0xFF67D2FF)
+val AmoPink = Color(0xFFF16AB5)
+val AmoViolet = Color(0xFF8C74FF)
+val AmoGreen = Color(0xFF78E6AD)
+val AmoAmber = Color(0xFFFFC56E)
